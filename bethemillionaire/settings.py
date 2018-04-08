4a486a97c596a932cf57be33797b2a8f4f7e04d9
Mubarak <mubarak117136@gmail.com>
2018-04-09 00:50:21 +0600

    update

--- bethemillionaire/settings.py
+++ bethemillionaire/settings.py
@@ -120,8 +120,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-print(BASE_DIR)
-
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'bethemillionaire/media')
