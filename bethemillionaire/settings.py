08e58c5a1744031f43f400bfad881bea23d32014
Mubarak <mubarak117136@gmail.com>
2018-04-09 20:59:21 +0600

    add

--- bethemillionaire/settings.py
+++ bethemillionaire/settings.py
@@ -222,3 +222,13 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = 'account.UserProfile'
 
 SECURE_SSL_REDIRECT = True
+
+CORS_REPLACE_HTTPS_REFERER      = True
+HOST_SCHEME                     = "https://"
+SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT             = True
+SESSION_COOKIE_SECURE           = True
+CSRF_COOKIE_SECURE              = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
+SECURE_HSTS_SECONDS             = 1000000
+SECURE_FRAME_DENY               = True
