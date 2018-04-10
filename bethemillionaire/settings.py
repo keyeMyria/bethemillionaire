eddf1929e9268cf5827e71cdbc498be67e896255
Mubarak <mubarak117136@gmail.com>
2018-04-10 13:51:04 +0600

    update settings

--- bethemillionaire/settings.py
+++ bethemillionaire/settings.py
@@ -223,6 +223,7 @@ AUTH_USER_MODEL = 'account.UserProfile'
 
 #SECURE_SSL_REDIRECT = True
 
+"""
 CORS_REPLACE_HTTPS_REFERER      = True
 HOST_SCHEME                     = "https://"
 SECURE_PROXY_SSL_HEADER         = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -232,3 +233,4 @@ CSRF_COOKIE_SECURE              = True
 SECURE_HSTS_INCLUDE_SUBDOMAINS  = True
 SECURE_HSTS_SECONDS             = 1000000
 SECURE_FRAME_DENY               = True
+"""
