af73bfda16d8aa26d72684253103a7fe095a2cbc
Mubarak <mubarak117136@gmail.com>
2018-04-24 00:29:46 +0600

    fix

--- account/views.py
+++ account/views.py
@@ -28,10 +28,45 @@ def logout_request(request):
 
 
 #pre registration
+import requests
+import simplejson
+import socket
+
 class Preregistration(View):
     template_name = 'account/preregistration.html'
 
     def get(self, request):
+
+
+        campaign_id = '92378201'
+        api_key = '75a678181ff9476176fee468f8ef587a'
+
+        ipaddress = socket.gethostbyname(socket.gethostname())
+
+        c = {
+                'campaignId': campaign_id,
+            }
+
+        data = {
+            'email': 'mubarak117136@gmail.com',
+            'campaign': c,
+            'ipAddress': ipaddress,
+        }
+
+        data_json = simplejson.dumps(data)
+
+        api_format = 'api-key %s' %api_key
+
+        headers = {
+            'Content-Type': 'application/json',
+            'X-Auth-Token': api_format,
+        }
+
+        r = requests.post('https://api.getresponse.com/v3/contacts', headers=headers, data=data_json)
+
+
+
+
         form = PreregistrationForm()
 
         affiliate_name = request.GET.get("userid")
