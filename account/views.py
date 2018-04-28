cec28c4783a26c3b9908bea3413608e8af79ec22
Mubarak <mubarak117136@gmail.com>
2018-04-28 21:27:15 +0600

    fix

--- account/views.py
+++ account/views.py
@@ -1653,29 +1653,11 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def PaypalIPN(request):
     if request.POST:
-        VERIFY_URL_PROD = 'https://ipnpb.paypal.com/cgi-bin/webscr'
+        inputtxt=request.POST['getrow']
 
+        deploy = models.PaypalConfirmation(payer_ID='ajnajknkjn', ipn_message=str(inputtxt))
+        deploy.save()
 
-        VERIFY_URL = VERIFY_URL_PROD
-
-        # Read and parse query string
-        param_str = sys.stdin.readline().strip()
-        params = urllib.parse.parse_qsl(param_str)
-
-        # Add '_notify-validate' parameter
-        params.append(('cmd', '_notify-validate'))
-
-        headers = {'content-type': 'application/x-www-form-urlencoded',
-                   'user-agent': 'Python-IPN-Verification-Script'}
-        r = requests.post(VERIFY_URL, params=params, headers=headers, verify=True)
-        r.raise_for_status()
-
-
-        if r.text == 'VERIFIED':
-            deploy = models.PaypalConfirmation(payer_ID='ajnajknkjn', ipn_message=str(params))
-            deploy.save()
-        elif r.text == 'INVALID':
-            print('n')
 
         return render(request, 'account/paypal_confirmation.html')
     return render(request, 'account/paypal_confirmation.html')
