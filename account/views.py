aa005cdf8fb18077d06039f52d3f92fdaaa3d846
Mubarak <mubarak117136@gmail.com>
2018-04-28 22:23:22 +0600

    fix

--- account/views.py
+++ account/views.py
@@ -1695,10 +1695,10 @@ def PaypalIPN(request):
         ipn_track_id = request.POST.get('ipn_track_id')
 
 
-        data.append(mc_gross, protection_eligibility, address_status, payer_id, address_street, payment_date, payment_status, charset, address_zip, first_name, mc_fee, address_country_code, address_name, notify_version, payer_status, business)
+        #data.append(mc_gross, protection_eligibility, address_status, payer_id, address_street, payment_date, payment_status, charset, address_zip, first_name, mc_fee, address_country_code, address_name, notify_version, payer_status, business)
 
 
-        deploy = models.PaypalConfirmation(payer_ID=payer_id, ipn_message=str(data))
+        deploy = models.PaypalConfirmation(payer_ID=payer_id, ipn_message=str(ipn_track_id))
         deploy.save()
 
 
