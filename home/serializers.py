ec717d73918302ec34ebc52c09ff933442a732d4
Mubarak <mubarak117136@gmail.com>
2018-06-09 13:45:49 +0600

    fix live

--- home/serializers.py
+++ home/serializers.py
@@ -3,8 +3,8 @@ from . import models
 
 
 class LiveChatMessageSerializers(serializers.ModelSerializer):
-    user = serializers.CharField(source='user.username')
+    chat_user = serializers.CharField(source='user.username')
 
     class Meta:
         model = models.LiveChatMessage
-        fields = ('room', 'message', 'user')
+        fields = ('room', 'message', 'user', 'chat_user')
